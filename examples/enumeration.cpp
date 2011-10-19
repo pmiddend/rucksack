abc0ae82ff6aaf99259e8c9b91b6dd73ef219fc7
phillemann <pmidden@gmx.net>
2011-10-19 19:41:50 +0200

    Improved test for enumeration widget

--- examples/enumeration.cpp
+++ examples/enumeration.cpp
@@ -7,8 +7,16 @@
 #include <sge/image/color/any/object.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/io/cerr.hpp>
+#include <fcppt/random/uniform.hpp>
+#include <fcppt/random/default_generator.hpp>
+#include <fcppt/random/make_inclusive_range.hpp>
+#include <fcppt/chrono/high_resolution_clock.hpp>
+#include <fcppt/chrono/duration_impl.hpp>
+#include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/container/ptr/push_back_unique_ptr.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <cstdlib>
+#include <boost/ptr_container/ptr_vector.hpp>
 #include <fcppt/config/external_end.hpp>
 
 int main()
@@ -31,34 +39,61 @@ try
 	viewport_box.child(
 		enumeration_box);
 
-	rucksack::widget::dummy inner_sprite(
-		rucksack::axis_policy2(
-			rucksack::axis_policy(
-				rucksack::minimum_size(
-					100),
-				rucksack::preferred_size(),
-				rucksack::is_expanding(
-					false)),
-			rucksack::axis_policy(
-				rucksack::minimum_size(
-					50),
-				rucksack::preferred_size(),
-				rucksack::is_expanding(
-					false)),
-			rucksack::aspect(
-				1,
-				1)));
+	fcppt::random::default_generator def_gen(
+		static_cast<fcppt::random::default_generator::result_type>(
+			fcppt::chrono::high_resolution_clock::now().time_since_epoch().count()));
+
+	fcppt::random::uniform<rucksack::scalar,fcppt::random::default_generator &>
+		size_rng_w(
+			fcppt::random::make_inclusive_range(
+				10,
+				300),
+			def_gen),
+		size_rng_h(
+			fcppt::random::make_inclusive_range(
+				10,
+				300),
+			def_gen);
+
+	typedef
+	boost::ptr_vector<rucksack::widget::dummy>
+	dummy_sprite_sequence;
+
+	dummy_sprite_sequence sprites;
 
 	testbed_.add_widget(
 		viewport_box,
 		sge::image::colors::blue());
 
-	testbed_.add_widget(
-		inner_sprite,
-		sge::image::colors::red());
+	for(unsigned i = 0; i < 20; ++i)
+	{
+		fcppt::container::ptr::push_back_unique_ptr(
+			sprites,
+			fcppt::make_unique_ptr<rucksack::widget::dummy>(
+				rucksack::axis_policy2(
+					rucksack::axis_policy(
+						rucksack::minimum_size(
+							size_rng_w()),
+						rucksack::preferred_size(),
+						rucksack::is_expanding(
+							false)),
+					rucksack::axis_policy(
+						rucksack::minimum_size(
+							size_rng_h()),
+						rucksack::preferred_size(),
+						rucksack::is_expanding(
+							false)),
+					rucksack::aspect(
+						1,
+						1))));
 
-	enumeration_box.push_back_child(
-		inner_sprite);
+		enumeration_box.push_back_child(
+			sprites.back());
+
+		testbed_.add_widget(
+			sprites.back(),
+			sge::image::colors::red());
+	}
 
 	testbed_.run();
 }
